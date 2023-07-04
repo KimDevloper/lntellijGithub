b438983887ddb5a2e0e3438d6cc0e19368e41756
KimDevloper <a62387272@gmail.com>
2023-07-04 18:20:25 +0900

    [Comment] Spring Project test 설명 주석 추가

--- src/main/java/me/kimdaewon/springbootdeveloper/Member.java
+++ src/main/java/me/kimdaewon/springbootdeveloper/Member.java
@@ -6,8 +6,8 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 
-@NoArgsConstructor // TODO 해석 필요
-@AllArgsConstructor // TODO 해석 필요
+@NoArgsConstructor // TODO 해석 필요 여기에 필드에 쓴 모든생성자만 만들어줌
+@AllArgsConstructor // TODO 해석 필요 기본 생성자를 만들어줌
 @Getter // 게터 함수를 만들어준다.
 @Entity // 객체클래스를 orm(jpa)를 통해서 db table를 생성
 public class Member { // 여기가 엔티티의 틀 -> 디비 테이블과 객체가 맵핑된다.
--- src/main/java/me/kimdaewon/springbootdeveloper/SpringBootDeveloperApplication.java
+++ src/main/java/me/kimdaewon/springbootdeveloper/SpringBootDeveloperApplication.java
@@ -3,7 +3,7 @@ package me.kimdaewon.springbootdeveloper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication // 스프링부트어플리케이션으로만 볼 때는 큰 기능이 없지만 어노테이션으로 본다면 다양한 기능있다.
 public class SpringBootDeveloperApplication {
     public static void main(String[] args) {
         SpringApplication.run(SpringBootDeveloperApplication.class, args);
